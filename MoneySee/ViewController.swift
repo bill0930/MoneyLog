cadedc8d81878f54f992c244d704f81f1fadaef8
Billy Chan <bill0930@gmail.com>
2021-04-02 20:01:57 +0800

    Initial Commit

---
+++ MoneySee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoneySee
+//
+//  Created by Billy Chan on 2/4/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
